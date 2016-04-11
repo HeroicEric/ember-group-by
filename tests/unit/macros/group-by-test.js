2b2d7c952cf03ef415a00c914705339b9e7cf242
Federico Teotini <templarblack.fed03@gmail.com>
2016-04-11 13:10:57 +0200

    refactored test

--- tests/unit/macros/group-by-test.js
+++ tests/unit/macros/group-by-test.js
@@ -2,33 +2,26 @@ import Ember from 'ember';
 import { module, test } from 'qunit';
 import groupBy from 'ember-group-by/macros/group-by';
 
-var cars, car1, car2, car3, car4, car5, dealership;
-
-module('Unit - groupBy', {
-  setup: function() {
-    car1 = { name: 'Carrera', color: 'red' };
-    car2 = { name: 'Veyron', color: 'red' };
-    car3 = { name: 'Corvette', color: 'blue' };
-    car4 = { name: 'Viper', color: 'blue' };
-    car5 = { name: 'Cobra', color: 'green' };
-
-    cars = [car1, car2, car3, car4, car5];
-
-    dealership = Ember.Object.extend({
-      cars: cars,
-      carsGroupedByColor: groupBy('cars', 'color')
-    }).create();
-  }
-});
+module('Unit - groupBy');
 
 test('it groups cars by color', function(assert) {
   assert.expect(1);
+  var car1 = { name: 'Carrera', color: 'red' };
+  var car2 = { name: 'Veyron', color: 'red' };
+  var car3 = { name: 'Corvette', color: 'blue' };
+  var car4 = { name: 'Viper', color: 'blue' };
+  var car5 = { name: 'Cobra', color: 'green' };
+  var dealership = Ember.Object.extend({
+    cars: [car1, car2, car3, car4, car5],
+    carsGroupedByColor: groupBy('cars', 'color')
+  }).create();
+
   var redGroup = { property: 'color', value: 'red', items: [car1, car2] };
   var blueGroup = { property: 'color', value: 'blue', items: [car3, car4] };
   var greenGroup = { property: 'color', value: 'green', items: [car5] };
 
-  var result = dealership.get('carsGroupedByColor');
   var expected = [redGroup, blueGroup, greenGroup];
+  var result = dealership.get('carsGroupedByColor');
 
   assert.deepEqual(result, expected);
 });
