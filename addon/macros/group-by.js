8aa821172eb2034b92edc025705dacca83018814
Maciej Kwaśniak <kmaciek17@gmail.com>
2016-11-10 10:06:16 +0100

    Fix empty array

--- addon/macros/group-by.js
+++ addon/macros/group-by.js
@@ -12,17 +12,19 @@ export default function groupBy(collection, property) {
     var groups = new A();
     var items = get(this, collection);
 
-    items.forEach(function(item) {
-      var value = get(item, property);
-      var group = groups.findBy('value', value);
+    if (items) {
+      items.forEach(function(item) {
+        var value = get(item, property);
+        var group = groups.findBy('value', value);
 
-      if (isPresent(group)) {
-        get(group, 'items').push(item);
-      } else {
-        group = { property: property, value: value, items: [item] };
-        groups.push(group);
-      }
-    });
+        if (isPresent(group)) {
+          get(group, 'items').push(item);
+        } else {
+          group = { property: property, value: value, items: [item] };
+          groups.push(group);
+        }
+      });
+    }
 
     return groups;
   }).readOnly();
